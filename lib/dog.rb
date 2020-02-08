7afccf87862715f5120f639fcba6fdaeb42b970b
sasha walker <sashawalker31@gmail.com>
2020-02-08 15:39:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,19 @@
 class Dog 
-  def initialize(name)
-    @name = name
+ def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-  
-  def dog=(name)
-    @name = name
-  end 
-  
-  def dog
-    @name 
-  end 
-  
-  def breed=(breed)
-    @breed = breed
-  end 
-  
-  def breed 
-    @breed 
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
   end 
 end 
 
